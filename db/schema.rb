7b21ad130506d545567e4700f58602265538e2c2
Raja <rmnforever@gmail.com>
2017-02-13 22:28:12 +0530

    Migrate from sqlite3 to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170205135822) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "doctors", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
